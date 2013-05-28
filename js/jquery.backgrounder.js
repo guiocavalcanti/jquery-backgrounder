2c8a07682d9bc2bcec92fad439b07d8be01dd5dd
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-05-28 09:09:57 -0300

    Minor refactors

--- js/jquery.backgrounder.js
+++ js/jquery.backgrounder.js
@@ -14,48 +14,76 @@ $.fn.backgrounder = function( images, options )
 	var settings = {
 		transitionTime: 1600,						// Amount of time to fade in/out the images
 		displayTime: 		5000,
-		zIndex: 				-666
+		zIndex: 				-666,
+    width: $(this).width(),
+    height: $(this).height(),
 	};
+
+  var helper = {
+    buildContainer: function(){
+      var $container = $("<div/>").attr("id", "backgrounder");
+      var css = {
+        left: 0,
+        top: 0,
+        overflow: "hidden",
+        zIndex:settings.zIndex,
+        width: '100%'
+      }
+
+      return $container.css(css);
+    },
+
+    buildImage: function(src) {
+      var css = {
+        display:"none",
+        position:"absolute",
+        zIndex:settings.zIndex,
+        width: '100%',
+        height: settings.height
+      }
+
+      return $("<div>").css(css).addClass('image').addClass(src);
+    }
+  }
 
 	// Combine options with default settings if it's needed
 	if(options && typeof options == "object")
 		settings = $.extend(settings, options);
 
-	// Create container and add to the body
-	var container = $("<div/>").attr("id", "backgrounder").css({left: 0, top: 0, overflow: "hidden", zIndex:settings.zIndex});
+	// Create $container and add to the body
+	var $container = helper.buildContainer();
   var $this = $(this);
 
-	$(this).prepend(container).css({overflow:"hidden"});
+	$(this).prepend($container).css({overflow:"hidden"});
 
-	// Load all the images and add to container
-	var imgElement;
+	// Load all the images and add to $container
+	var $imgElement;
 	for( var i = images.length; i--; )
 	{
-    var url = "url(" + images[i] + ")";
-	  imgElement = $("<div>").css({display:"none",position:"absolute",zIndex:settings.zIndex, width: $this.width(), height: $this.height()}).addClass(images[i]).addClass("image");
-		container.append(imgElement);
+    var $imgElement = helper.buildImage(images[i]);
+		$container.append($imgElement);
 	}
 
-	// Grab the first image in the container and fade in
-	$("div:first-child", container).toggleClass("current").fadeIn(settings.transitionTime);
+	// Grab the first image in the $container and fade in
+	$("div:first-child", $container).toggleClass("current").fadeIn(settings.transitionTime);
 
 	// Slideshow enabled
 	setInterval(function()
 	{
-		var currImg = $(".current", container);
+		var $currImg = $(".current", $container);
 
-		if( !currImg.next().length )
+		if( !$currImg.next().length )
 			var startAtBeginning = true;
 
 		if( startAtBeginning )
 		{
-			container.find('div:first-child').addClass("current").css({display:'block'});
-			currImg.removeClass("current").fadeOut(settings.transitionTime);
+			$container.find('div:first-child').addClass("current").css({display:'block'});
+			$currImg.removeClass("current").fadeOut(settings.transitionTime);
 		}
 		else
 		{
-			currImg.next().addClass("current").fadeIn(settings.transitionTime,function(){
-				currImg.removeClass("current").css({display:"none"});
+			$currImg.next().addClass("current").fadeIn(settings.transitionTime,function(){
+				$currImg.removeClass("current").css({display:"none"});
 			});
 		}
 	}, settings.displayTime);
